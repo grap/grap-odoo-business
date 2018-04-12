04cc13ac1b3c0fb875e6a2f1c87339ece45e6d65
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-04-12 14:13:23 +0200

    [WIP] refactor product_to_scale_bizerba

---
+++ product_to_scale_bizerba/README.rst
@@ -0,0 +1,57 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=================================================
+Product - Send to Bizerba Scales (Retail Connect)
+=================================================
+
+
+This module extends the functionality of ... to support ...
+and to allow you to ...
+
+Installation
+============
+
+To install this module, you need to:
+
+#. Do this ...
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+* Go to ...
+
+.. figure:: /path/to/local/image.png
+   :width: 800 px
+
+Usage
+=====
+
+To use this module, you need to:
+
+* Go to ...
+
+Known issues / Roadmap
+======================
+
+* It should be great to split this module into many modules, with some generic
+  features in a module named product_to_scale and some specifics features
+  for each scales system. 'product_to_scale_bizerba',
+  'product_to_scale_metler_toledo', etc.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain LE GAL <https://twitter.com/legalsylvain>
+
+Funders
+-------
+
+* GRAP, Groupement Régional Alimentaire de Proximité <http://www.grap.coop>
---
+++ product_to_scale_bizerba/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product - Send to Bizerba Scales (Retail Connect)',
+    'summary': 'Synchronize Odoo database with Retail Connect Bizerba System',
+    'version': '8.0.2.0.0',
+    'category': 'Product',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'security/ir_module_category.xml',
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'security/ir_rule.xml',
+        'data/ir_config_parameter.xml',
+        'data/ir_cron.xml',
+        'views/view_product_product.xml',
+        'views/view_product_uom.xml',
+        'views/view_product_scale_system.xml',
+        'views/view_product_scale_group.xml',
+        'views/view_product_scale_log.xml',
+        'views/view_product_scale_log_wizard.xml',
+        'views/action.xml',
+        'views/menu.xml',
+    ],
+    'demo': [
+        'demo/res_groups.xml',
+        'demo/product_uom.xml',
+        'demo/product_scale_system.xml',
+        'demo/product_scale_system_product_line.xml',
+        'demo/product_scale_group.xml',
+        'demo/product_product.xml',
+        'demo/decimal_precision.xml',
+    ],
+}
