bb5e62b877a2dcf727f130b87779971d769ab699
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-04-24 12:39:14 +0200

    [IMP] add security on commission product

--- recurring_consignment/models/account_tax_code.py
+++ recurring_consignment/models/account_tax_code.py
@@ -3,7 +3,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
 
 
 class AccountTaxCode(models.Model):
@@ -17,3 +18,11 @@ class AccountTaxCode(models.Model):
         "If not set, transaction will not be commissioned. (this case is"
         " usefull to avoid to commission taxes transaction, because in"
         " most cases, commissions are computed on without taxes amount).")
+
+    @api.constrains('consignment_product_id')
+    def _check_consignment_product_id(self):
+        for tax_code in self.filtered(lambda x: x.consignment_product_id):
+            if not tax_code.consignment_product_id.is_consignment_commission:
+                raise UserError(_(
+                    "Set only consignement commission product in the"
+                    " according field"))
