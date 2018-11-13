9b0ccce64a24e397639c8699d1ca6b737463252c
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-11-13 13:15:47 +0100

    [REF] move account_move_change_number from -grap into -business

---
+++ account_move_change_number/__openerp__.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Move Change Number Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Account - Move Change Number',
+    'version': '1.0',
+    'category': 'Accounting',
+    'description': """
+Allow special user to rename account move
+=========================================
+
+Features
+--------
+
+* Allow special user to rename account move ;
+  Usefull if the account move doesn't have the good numeration.
+  (for exemple if you change account move sequence) ;
+
+Technical informations
+----------------------
+
+* change account move name with next sequence ;
+* add in the field 'narration' the old name ;
+
+Copyright, Author and Licence
+-----------------------------
+
+* Copyright : 2014, Groupement Régional Alimentaire de Proximité;
+* Author
+    * Sylvain LE GAL (https://twitter.com/legalsylvain);
+* Licence : AGPL-3 (http://www.gnu.org/licenses/)
+    """,
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'security/res_groups.yml',
+        'views/account_move_view.xml',
+    ],
+}
---
+++ account_move_change_number/model/account_move.py
@@ -0,0 +1,58 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Move Change Number Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from datetime import datetime
+
+from openerp.osv.orm import Model
+from openerp.tools.translate import _
+
+
+class account_move(Model):
+    _inherit = 'account.move'
+
+    def rename_account_move_change_number(self, cr, uid, ids, context=None):
+        user_obj = self.pool['res.users']
+        for id in ids:
+            old_account_move = self.browse(cr, uid, id, context=context)
+            old_name = old_account_move.name
+            old_narration = old_account_move.narration or ''
+
+            # unpost acount move
+            self.button_cancel(cr, uid, [id], context=context)
+
+            # set name to "/"
+            self.write(cr, uid, [id], {'name': '/'}, context=context)
+
+            # post account move
+            self.post(cr, uid, [id], context=context)
+            new_name = self.browse(cr, uid, id, context=context).name
+
+            # Add description of the change
+            date = datetime.today().strftime("%d/%m/%Y")
+            author = user_obj.browse(cr, uid, uid, context=context).name
+            self.write(cr, uid, [id], {
+                'narration': old_narration + _(
+                    "\nAccount move renamed. Old name : %s."
+                    " New name : %s. Rename date : %s. Author : %s.") % (
+                    old_name, new_name, date, author)
+            }, context=context)
+        return True
