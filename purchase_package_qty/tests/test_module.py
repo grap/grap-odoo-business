ba96fba091fb632c17b35d8722912cf5730789bf
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-22 18:11:33 +0100

    fixup! fixup! fixup! [MIG] purchase_package_qty: Migration to 12.0

--- purchase_package_qty/tests/test_module.py
+++ purchase_package_qty/tests/test_module.py
@@ -12,7 +12,7 @@ class TestModule(TransactionCase):
 
         self.unit_uom = self.env.ref('uom.product_uom_unit')
         self.supplier_with_package = self.env.ref('base.res_partner_1')
-        self.supplier_without_package = self.env.ref('base.res_partner_2')
+        self.supplier_without_package = self.env.ref('base.res_partner_3')
         self.product = self.env.ref(
             'purchase_package_qty.product_product_package_6')
         self.purchase_order = self.env.ref(
@@ -20,13 +20,12 @@ class TestModule(TransactionCase):
         self.purchase_order_line = self.env.ref(
             "purchase_package_qty.purchase_order_line")
 
-    # Test Section
     def test_01_purchase_order_line_onchange_package_strict(self):
         # [Functional Test] Check if onchange function changes
         # quantity (strict context)
         self.purchase_order.partner_id = self.supplier_with_package
         self.purchase_order_line.product_qty = 5
-        self.purchase_order_line.onchange_product_id()
+        self.purchase_order_line._onchange_quantity()
         self.assertEqual(
             self.purchase_order_line.product_qty, 6,
             "On change with package defined should round quantity")
@@ -36,7 +35,7 @@ class TestModule(TransactionCase):
         # quantity (no package context)
         self.purchase_order.partner_id = self.supplier_without_package
         self.purchase_order_line.product_qty = 5
-        self.purchase_order_line.onchange_product_id()
+        self.purchase_order_line._onchange_quantity()
         self.assertEqual(
             self.purchase_order_line.product_qty, 5,
             "On change without package defined should not change quantity")
