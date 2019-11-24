994e25c4a45dc9f4f7a7d289be7fcb2ba166d9e0
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-24 15:08:52 +0100

    [MIG] account_move_change_number: Migration to 12.0

--- account_move_change_number/models/account_move.py
+++ account_move_change_number/models/account_move.py
@@ -1,12 +1,11 @@
 
-# coding: utf-8
 # Copyright (C) 2013 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from datetime import datetime
 
-from openerp import _, api, models
+from odoo import _, api, models
 
 
 class AccountMove(models.Model):
