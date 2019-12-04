473ec9b4bce0883e3724f1cc7dcf065ddcd6cb50
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-12-04 13:38:17 +0100

    [MIG] recurring_consignment: Migration to 12.0

---
+++ recurring_consignment_pos/models/pos_order.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    @api.constrains('partner_id')
+    def _check_partner_id_recurring_consignment(self):
+        if self.partner_id.is_consignor:
+            raise UserError(_("You can not make PoS Orders to consignors"))
