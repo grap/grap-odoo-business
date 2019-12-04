8ea7e0e23e2366b844957b1fa0c6149880ad8311
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-12-04 13:38:17 +0100

    [MIG] recurring_consignment: Migration to 12.0

---
+++ recurring_consignment_sale/models/product_template.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.multi
+    def _check_consignor_changes(self, vals):
+        super()._check_consignor_changes(vals)
+        SaleOrderLine = self.env['sale.order.line']
+        if vals.get('consignor_partner_id', False):
+            for template in self:
+                product_ids = template.product_variant_ids.ids
+                if template.consignor_partner_id.id !=\
+                        vals.get('consignor_partner_id', False):
+                    order_lines = SaleOrderLine.search([
+                        ('product_id', 'in', product_ids)])
+                    if len(order_lines):
+                        raise UserError(_(
+                            "You can not change the value of the field"
+                            " 'Consignor' because the product is associated"
+                            " to one or more Sale Order Lines. You should"
+                            " disable the product and create a new one."))
---
+++ recurring_consignment_sale/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module is a glue module between ``recurring_consignment`` and
+``sale`` module.
+
+Once installed, it will block the creation of sales for customers
+flagged as consignors.
