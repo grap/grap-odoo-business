a9f8f6d19f775596f829d2044e2e44d8d9504d37
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-04-06 22:18:18 +0200

    WIP - refactor login

--- sale_eshop/models/res_partner.py
+++ sale_eshop/models/res_partner.py
@@ -18,7 +18,7 @@ class ResPartner(models.Model):
     _eshop_invalidation_type = 'single'
 
     _eshop_invalidation_fields = [
-        'name', 'lang', 'email', 'eshop_active', 'eshop_state',
+        'name', 'lang', 'email', 'eshop_state',
         'phone', 'mobile', 'street', 'street2', 'zip', 'city',
     ]
 
@@ -28,7 +28,6 @@ class ResPartner(models.Model):
     _ESHOP_STATE_SELECTION = [
         ('disabled', 'Disabled'),
         ('email_to_confirm', 'EMail To Confirm'),
-        ('first_purchase', 'First Purchase'),
         ('enabled', 'Enabled'),
     ]
 
@@ -38,18 +37,6 @@ class ResPartner(models.Model):
     eshop_state = fields.Selection(
         selection=_ESHOP_STATE_SELECTION, string='State on eShop',
         readonly=True, default='disabled')
-
-    eshop_active = fields.Boolean(
-        string='Can buy on eShop', store=True, compute='_compute_eshop_active',
-        readonly=True)
-
-    # Compute Section
-    @api.multi
-    @api.depends('eshop_state')
-    def _compute_eshop_active(self):
-        for partner in self:
-            partner.eshop_active =\
-                partner.eshop_state in ['first_purchase', 'enabled']
 
     # Overload Section
     @api.multi
@@ -64,47 +51,49 @@ class ResPartner(models.Model):
     # View - Section
     @api.multi
     def button_enable_eshop(self):
-        self.write({'eshop_state': 'enabled'})
+        self.write({
+            'eshop_state': 'enabled',
+        })
 
     @api.multi
     def button_disable_eshop(self):
         self.write({
-            'eshop_password': False,
             'eshop_state': 'disabled',
         })
 
     @api.multi
-    def button_generate_credentials(self):
+    def button_generate_send_credentials(self):
         self._generate_credentials()
+        self.send_credentials()
 
+    # Eshop API - Section
     @api.multi
-    def button_send_credentials(self):
+    def send_credentials(self):
         template = self.env.ref('sale_eshop.eshop_send_crendential_template')
         for partner in self:
             template.send_mail(partner.id, force_send=True)
         return True
 
-    # Eshop API - Section
     @api.model
     def login(self, login, password):
         ResUsers = self.env['res.users']
         if not password:
             return False
-        res = self.search([
+        partners = self.search([
             ('email', '=', login),
             ('eshop_password', '=', password),
-            ('eshop_state', 'in', ['first_purchase', 'enabled']),
+            ('eshop_state', 'in', ['enabled']),
         ])
-        if len(res) == 1:
-            return res[0].id
+        if len(partners) == 1:
+            return partners[0].id
         try:
             ResUsers.sudo().check_credentials(password)
-            res = self.search([
+            partners = self.search([
                 ('email', '=', login),
-                ('eshop_active', '=', True),
+                ('eshop_state', 'in', ['enabled']),
             ])
-            if len(res) == 1:
-                return res[0].id
+            if len(partners) == 1:
+                return partners[0].id
             else:
                 return False
         except exceptions.AccessDenied:
@@ -123,6 +112,26 @@ class ResPartner(models.Model):
         # Send an email
         return partner.button_send_credentials()
 
+    @api.model
+    def eshop_email_confirm(self, partner_id, email):
+        partners = self.browse([partner_id])
+        if not partners:
+            return "partner_not_found"
+
+        partner = partners[0]
+        if partner.email != email:
+            return "bad_email"
+
+        if partner.eshop_state in ['enabled']:
+            return "still_confirmed"
+
+        if partner.eshop_state in ['disabled']:
+            return "disabled"
+
+        # eshop_state is "email_to_confirm"
+        partner.write({"eshop_state": "enabled"})
+        return "enabled"
+
     # Private Section
     @api.multi
     def _generate_credentials(self):
@@ -131,10 +140,11 @@ class ResPartner(models.Model):
             password = ''.join(random.choice(
                 self._PASSWORD_CHARS) for i in range(self._PASSWORD_LENGTH))
             partner.write({
-                'eshop_password': password,
-                'eshop_state': 'enabled'})
+                "eshop_password": password,
+                "eshop_state": "email_to_confirm",
+            })
 
     # Overwrite section
     @api.model
     def _get_eshop_domain(self):
-        return [('eshop_active', '=', True)]
+        return [('eshop_state', 'in', ["email_to_confirm", "enabled"])]
