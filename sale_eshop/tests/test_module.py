b3e63123ee24fe051d500c074226f383fcc300f1
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-04-09 21:17:36 +0200

    fixup! fixup! fixup! [REM] remove unpacking options + [FIX] various fixes

--- sale_eshop/tests/test_module.py
+++ sale_eshop/tests/test_module.py
@@ -19,6 +19,8 @@ class TestModule(TransactionCase):
         self.apple = self.env.ref('sale_eshop.product_apple')
         self.product_disabled = self.env.ref('sale_eshop.product_disabled')
         self.product_not_available = self.env.ref('stock.product_icecream')
+        self.recovery_moment = self.env.ref(
+            "sale_recovery_moment.recovery_moment_1")
 
     # Test Section
     def test_01_login(self):
@@ -60,17 +62,16 @@ class TestModule(TransactionCase):
         # Create Order
         self.SaleOrder.eshop_set_quantity(
             self.customer.id, self.banana.id, 3, 'add')
-        order_id = self.SaleOrder.eshop_get_current_sale_order_id(
+        order = self.SaleOrder.eshop_get_current_sale_order(
             self.customer.id)
         self.assertNotEqual(
-            order_id, False,
+            order, False,
             "Adding a product for a customer that don't have sale order"
             " should create a new sale order")
 
         # Add quantity to the same product
         self.SaleOrder.eshop_set_quantity(
             self.customer.id, self.banana.id, 2, 'add')
-        order = self.SaleOrder.browse(order_id)
         order_line = order.order_line[0]
         self.assertEqual(
             order_line.product_uom_qty, 5,
@@ -83,15 +84,15 @@ class TestModule(TransactionCase):
             order_line.product_uom_qty, 1,
             "setting a quantity should erase previous quantity")
 
-        # TODO, select a recovery moment
-        # # Finish the order
-        # order.eshop_set_as_sent()
-        # self.assertEqual(
-        #     order.state, 'sent',
-        #     "Finishing an order in the eshop should set the order as 'sent'")
+        # Select a recovery moment
+        self.SaleOrder.eshop_select_recovery_moment(
+            self.customer.id, self.recovery_moment.id)
+        self.assertEqual(
+            order.state, 'sent',
+            "Finishing an order in the eshop should set the order as 'sent'")
 
-        # # Simulate cron
-        # self.SaleOrder.sudo()._eshop_cron_confirm_orders()
-        # self.assertNotEqual(
-        #     order.state, 'sent',
-        #     "Once cron has been run, orders should not be in 'sent' state.")
+        # Simulate cron
+        self.SaleOrder.sudo()._eshop_cron_confirm_orders()
+        self.assertNotEqual(
+            order.state, 'sent',
+            "Once cron has been run, orders should not be in 'sent' state.")
