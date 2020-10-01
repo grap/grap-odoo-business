bd850a9fd8445f4c2f8dd55599f7605cef3377ff
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-10-01 11:33:33 +0200

    [IMP] product_notation: migration of spider_chart_image

---
+++ product_notation/migrations/12.0.1.0.1/pre-migration.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+column_renames = {
+    'product_product': [
+        ('spider_chart_image', None),
+    ],
+}
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_columns(env.cr, column_renames)
