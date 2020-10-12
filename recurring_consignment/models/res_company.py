3cba0fc09d3e347711420594bcc21f658aa224ac
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-10-12 20:31:30 +0200

    [FIX] recurring_consignement ; [ADD] sequence on consignors ; [ADD] glue module with fiscal_company_base

---
+++ recurring_consignment/models/res_company.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import logging
+
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        res._create_consignor_sequence()
+
+    @api.multi
+    def _create_consignor_sequence(self):
+        ResPartner = self.env["res.partner"]
+        Irsequence = self.env["ir.sequence"]
+        for company in self:
+            _logger.info(
+                "Creating consignor sequence for company '%s'" % (
+                    company.name))
+            current_consignor_qty = len(
+                ResPartner.with_context(active_test=False).search([
+                    ('company_id', '=', company.id),
+                    ('is_consignor', '=', True),
+                ]))
+            Irsequence.create(
+                company._prepare_consignor_sequence(current_consignor_qty)
+            )
+
+    @api.multi
+    def _prepare_consignor_sequence(self, current_consignor_qty):
+        self.ensure_one()
+        return {
+            "name": "Consignor sequence",
+            "code": "consignor.create.wizard",
+            "implementation": "standard",
+            "company_id": self.id,
+            "prefix": "Prod",
+            "padding": 3,
+            "number_next_actual": current_consignor_qty + 1,
+        }
