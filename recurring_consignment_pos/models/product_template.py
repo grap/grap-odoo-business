14aa54e3f26f94abf452b03bbf91f7c38a7274a0
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-16 20:20:57 +0100

    [BLACK]

--- recurring_consignment_pos/models/product_template.py
+++ recurring_consignment_pos/models/product_template.py
@@ -7,30 +7,33 @@ from odoo.exceptions import Warning as UserError
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     # Constrains Section
-    @api.constrains('is_consignment_commission', 'available_in_pos')
+    @api.constrains("is_consignment_commission", "available_in_pos")
     def _check_is_consignment_commission_pos(self):
-        if self.filtered(
-                lambda x: x.is_consignment_commission and x.available_in_pos):
-            raise UserError(_(
-                "A Consignment Commission can not be available in PoS"))
+        if self.filtered(lambda x: x.is_consignment_commission and x.available_in_pos):
+            raise UserError(_("A Consignment Commission can not be available in PoS"))
 
     @api.multi
     def _check_consignor_changes(self, vals):
         super()._check_consignor_changes(vals)
-        PosOrderLine = self.env['pos.order.line']
-        if vals.get('consignor_partner_id', False):
+        PosOrderLine = self.env["pos.order.line"]
+        if vals.get("consignor_partner_id", False):
             for template in self:
                 product_ids = template.product_variant_ids.ids
-                if template.consignor_partner_id.id !=\
-                        vals.get('consignor_partner_id', False):
-                    order_lines = PosOrderLine.search([
-                        ('product_id', 'in', product_ids)])
+                if template.consignor_partner_id.id != vals.get(
+                    "consignor_partner_id", False
+                ):
+                    order_lines = PosOrderLine.search(
+                        [("product_id", "in", product_ids)]
+                    )
                     if len(order_lines):
-                        raise UserError(_(
-                            "You can not change the value of the field"
-                            " 'Consignor' because the product is associated"
-                            " to one or more PoS Order Lines. You should"
-                            " disable the product and create a new one."))
+                        raise UserError(
+                            _(
+                                "You can not change the value of the field"
+                                " 'Consignor' because the product is associated"
+                                " to one or more PoS Order Lines. You should"
+                                " disable the product and create a new one."
+                            )
+                        )
