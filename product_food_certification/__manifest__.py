233d7e9ca5e0316af95a7d14ad2c2f406e83d296
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-05 11:38:30 +0100

    [ADD] product_food_certification

---
+++ product_food_certification/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Products - Food Certification Informations",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-business",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+    "images": [],
+    "installable": True,
+}
