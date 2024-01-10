871d0e8e17e2c64059d091259d958ce8f95b4d4b
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-10 23:19:04 +0100

    [IMP] automatize uninstallation of obsolete module 'product_notation' and installation of new module 'product_food_certification'

--- product_food/migrations/12.0.2.0.0/pre-migration.py
+++ product_food/migrations/12.0.2.0.0/pre-migration.py
@@ -2,7 +2,12 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
 
 column_renames = {
     "product_product": [
@@ -13,6 +18,28 @@ column_renames = {
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
+    _logger.info("FORCE UNINSTALL 'product_notation' ...")
+    openupgrade.logged_query(
+        env.cr,
+        """
+    UPDATE ir_module_module
+    SET state = 'to remove'
+    WHERE name = 'product_notation'
+    AND state != 'uninstalled';
+    """,
+    )
+
+    _logger.info("FORCE INSTALL 'product_food_certification' ...")
+    openupgrade.logged_query(
+        env.cr,
+        """
+    UPDATE ir_module_module
+    SET state = 'to install'
+    WHERE name = 'product_food_certification'
+    AND state != 'installed';
+    """,
+    )
+
     openupgrade.rename_columns(env.cr, column_renames)
 
 
