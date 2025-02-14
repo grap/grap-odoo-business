e8a6873e54ca2910f8f5a6ba54b14ead914feed5
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-14 10:59:41 +0100

    [ADD] similar glue module partner_hide_technical_employee_calendar

---
+++ partner_hide_technical_employee_calendar/models/res_partner.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    def _compute_meeting(self):
+        return super(
+            ResPartner, self.with_context(show_odoo_employee=True)
+        )._compute_meeting()
