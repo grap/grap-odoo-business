d0b8f5b7167588631371a5d3692b0e97e53d82f7
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-14 16:22:01 +0100

    [ADD] new module partner_hide_technical_abstract and related changes in all modules [REF] Add partner_hide_technical_fix_calendar and partner_hide_technical_fix_calendar_test. So removing, partner_hide_technical_user_calendar, partner_hide_technical_employee_calendar and partner_hide_technical_company_calendar

---
+++ partner_hide_technical_abstract/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Hide Technical Partners - Abstract Module",
+    "summary": "Technical module, used to to Hide partners created when creating"
+    " other items in Odoo",
+    "version": "16.0.1.0.0",
+    "category": "base",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-business",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "installable": True,
+}
