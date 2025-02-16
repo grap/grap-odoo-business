4757d66e62aab56fb0a1ec9855107c6fbdf261d2
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-16 21:12:35 +0100

    [ADD] hr_direct_address_home: new module to access and modify directly on employee form view, all the data relative to 'private partner'

---
+++ hr_direct_address_home/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Employee - Direct Access to home information",
+    "summary": "Prevent creation of many home partners at" " employee level.",
+    "version": "16.0.1.0.0",
+    "category": "base",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-business",
+    "license": "AGPL-3",
+    "depends": ["hr"],
+    "data": [
+        "views/view_hr_employee.xml",
+    ],
+    "installable": True,
+}
