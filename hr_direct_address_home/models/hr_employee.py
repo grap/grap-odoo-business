029ff8fbf9f7afd6aadd9e047165483106f4d73b
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-12-01 16:32:56 +0100

    [FIX] hr_direct_address_home: correct address_home_id field, when duplicating. + add Tests and improve coverage

--- hr_direct_address_home/models/hr_employee.py
+++ hr_direct_address_home/models/hr_employee.py
@@ -9,6 +9,8 @@ from odoo.tools import clean_context
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
+    address_home_id = fields.Many2one(copy=False)
+
     address_home_street = fields.Char(
         string="Street (Private)",
         compute="_compute_address_home_details",
@@ -86,49 +88,58 @@ class HrEmployee(models.Model):
         store=True,
     )
 
+    @api.model
+    def _get_address_home_fields(self):
+        return [
+            "street",
+            "street2",
+            "city",
+            "state_id",
+            "zip",
+            "country_id",
+            "phone",
+            "email",
+        ]
+
     @api.depends(
-        "address_home_id",
-        "address_home_id.street",
-        "address_home_id.street2",
-        "address_home_id.city",
-        "address_home_id.state_id",
-        "address_home_id.zip",
-        "address_home_id.country_id",
-        "address_home_id.phone",
-        "address_home_id.mobile",
-        "address_home_id.email",
+        lambda self: ["address_home_id"]
+        + [f"address_home_id.{x}" for x in self._get_address_home_fields()]
     )
     def _compute_address_home_details(self):
+        """Compute hr employee fields,
+        based on the data of the related address_home_id partner."""
         for employee in self:
             if employee.address_home_id:
-                employee.address_home_street = employee.address_home_id.street
-                employee.address_home_street2 = employee.address_home_id.street2
-                employee.address_home_city = employee.address_home_id.city
-                employee.address_home_state_id = employee.address_home_id.state_id
-                employee.address_home_zip = employee.address_home_id.zip
-                employee.address_home_country_id = employee.address_home_id.country_id
-                employee.address_home_phone = employee.address_home_id.phone
-                employee.address_home_mobile = employee.address_home_id.mobile
-                employee.address_home_email = employee.address_home_id.email
+                for field in self._get_address_home_fields():
+                    setattr(
+                        employee,
+                        f"address_home_{field}",
+                        getattr(employee.address_home_id, field),
+                    )
 
     def _inverse_address_home_details(self):
+        """Apply changes to Compute hr employee fields,
+        based on the data of the related address_home_id partner."""
+
         ResPartnerSudo = (
             self.env["res.partner"].sudo().with_context(**clean_context(self._context))
         )
         for employee in self:
-            vals = {
-                "street": employee.address_home_street,
-                "street2": employee.address_home_street2,
-                "city": employee.address_home_city,
-                "state_id": employee.address_home_state_id,
-                "zip": employee.address_home_zip,
-                "country_id": employee.address_home_country_id,
-                "phone": employee.address_home_phone,
-                "mobile": employee.address_home_mobile,
-                "email": employee.address_home_email,
-            }
+            vals = {}
+            for field in self._get_address_home_fields():
+                vals[field] = getattr(employee, f"address_home_{field}")
             if not employee.address_home_id:
                 vals["name"] = employee.name
                 employee.address_home_id = ResPartnerSudo.create(vals)
             else:
                 employee.address_home_id.sudo().write(vals)
+
+    @api.returns("self", lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        if self.address_home_id:
+            for field in self._get_address_home_fields():
+                current_value = getattr(self.address_home_id, field)
+                default[f"address_home_{field}"] = current_value
+        return super().copy(default)
