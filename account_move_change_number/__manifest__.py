c659012a40a048674d12f024f801d0abbd844458
Quentin Dupont <quentin.dupont@grap.coop>
2025-12-30 12:03:31 +0100

    [16.0][MIG] account_move_change_number

---
+++ account_move_change_number/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2013 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account - Move Change Number",
+    "version": "16.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Allow special user to rename account move",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-business",
+    "license": "AGPL-3",
+    "depends": [
+        "account_move_name_sequence",
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "views/view_account_move.xml",
+    ],
+    "demo": [
+        "demo/res_groups.xml",
+        "demo/account_journal.xml",
+        "demo/account_account.xml",
+        "demo/account_move.xml",
+    ],
+    "installable": True,
+}
---
+++ account_move_change_number/readme/DESCRIPTION.md
@@ -0,0 +1,10 @@
+That module allows special users to rename account move.
+
+Useful if the account move doesn't have the good numeration. (for
+exemple if you change account move sequence)
+
+**Technical informations**
+
+- change account move name with next sequence
+- add in the field 'narration' the old name
+- depends on account_move_name_sequence as sequence has disappeared since Odoo v14
