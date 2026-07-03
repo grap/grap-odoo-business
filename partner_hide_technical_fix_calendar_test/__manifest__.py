7c2a2fce54aa57c3b8297fd5a6cd1bb5dd9aa912
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-07-03 17:11:02 +0200

    [MIG] partner_hide_technical_fix_calendar_test: Migration to 18.0 (from 16.0)

--- partner_hide_technical_fix_calendar_test/__manifest__.py
+++ partner_hide_technical_fix_calendar_test/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Partner - Hide Partners - Test for Calendar Fix",
     "summary": "Test module",
-    "version": "16.0.2.0.2",
+    "version": "18.0.1.0.0",
     "category": "base",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-business",
